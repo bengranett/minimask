af2a4a3535eed7835ac0f91072e4296aa243fa71
Dida Markovic <dida.markovic@port.ac.uk>
2016-05-06 17:50:13 +0100

    Changes N->np for consistency (in sphere.py).

--- sphere.py
+++ sphere.py
@@ -1,11 +1,10 @@
 """spherical geometry utilities"""
 
-
-import numpy as N
+import numpy as np
 
 # degrees to radian conversions
-c = N.pi/180
-ic = 180/N.pi
+c = np.pi/180
+ic = 180/np.pi
 
 def distance(ra1,dec1,ra2,dec2):
     """ compute distance between two points on the sphere using the haversine formula
@@ -23,25 +22,25 @@ def distance(ra1,dec1,ra2,dec2):
     """
     dra = c*(ra1 - ra2)
     ddec = c*(dec1 - dec2)
-    r = N.sin(dra/2)**2 + N.cos(dec1*c)*N.cos(dec2*c)*N.sin(ddec/2)**2
-    return 2*N.arcsin(N.sqrt(r))*ic
+    r = np.sin(dra/2)**2 + np.cos(dec1*c)*np.cos(dec2*c)*np.sin(ddec/2)**2
+    return 2*np.arcsin(np.sqrt(r))*ic
 
 
 def lonlat2xyz(lon,lat,r=1):
     """ """
-    x = r*N.cos(lon*c)*N.cos(lat*c)
-    y = r*N.sin(lon*c)*N.cos(lat*c)
-    z = r*N.sin(lat*c)
+    x = r*np.cos(lon*c)*np.cos(lat*c)
+    y = r*np.sin(lon*c)*np.cos(lat*c)
+    z = r*np.sin(lat*c)
     return x,y,z
 
 def xyz2lonlat(x,y,z,getr=False):
     """ """
     if getr:
-        r = N.sqrt(x*x+y*y+z*z)
+        r = np.sqrt(x*x+y*y+z*z)
     else:
-        r = N.ones(x.shape)
-    lat = N.arcsin(z/r)*ic
-    lon = N.arctan2(y,x)*ic
+        r = np.ones(x.shape)
+    lat = np.arcsin(z/r)*ic
+    lon = np.arctan2(y,x)*ic
     if getr:
         return lon,lat,r
     return lon,lat
@@ -56,35 +55,35 @@ def rotate_xyz(x,y,z,angles=None,inverse=False):
     if angles==None:
         return x,y,z
 
-    xyz = N.array([x,y,z])
+    xyz = np.array([x,y,z])
     for dphi,dlon,dlat in angles:
         dphi*=c
         dlon*=c
         dlat*=c
-        m0 = N.array([[1,0,0],
-                      [0, N.cos(dphi),N.sin(dphi)],
-                      [0, -N.sin(dphi), N.cos(dphi)]])
+        m0 = np.array([[1,0,0],
+                      [0, np.cos(dphi),np.sin(dphi)],
+                      [0, -np.sin(dphi), np.cos(dphi)]])
 
-        m1 = N.array([[N.cos(dlon),-N.sin(dlon),0],
-                      [N.sin(dlon), N.cos(dlon),0],
+        m1 = np.array([[np.cos(dlon),-np.sin(dlon),0],
+                      [np.sin(dlon), np.cos(dlon),0],
                       [0,0,1]])
 
-        m2 = N.array([[N.cos(dlat),0,-N.sin(dlat)],
+        m2 = np.array([[np.cos(dlat),0,-np.sin(dlat)],
                       [0,1,0],
-                      [N.sin(dlat), 0, N.cos(dlat)]])
+                      [np.sin(dlat), 0, np.cos(dlat)]])
 
-        m = N.dot(N.dot(m1,m2),m0)
+        m = np.dot(np.dot(m1,m2),m0)
 
     if inverse:
-        m = N.linalg.inv(m)
+        m = np.linalg.inv(m)
         
-    xyz2 = N.dot(m,xyz)
+    xyz2 = np.dot(m,xyz)
     return xyz2
 
 def rotate_lonlat(lon,lat,angles=[(0,0)], inverse=False):
     """ Rotate a set of longitude and latitude coordinate pairs.
     """
-    xyz = N.array(lonlat2xyz(lon,lat))
+    xyz = np.array(lonlat2xyz(lon,lat))
     xyz2 = rotate_xyz(*xyz,angles=angles, inverse=inverse)
     return xyz2lonlat(*xyz2,getr=False)
 
@@ -97,21 +96,21 @@ def test_rotate_lonlat():
 
     print rotate_lonlat(10,20,angles=[(5,10,20)],inverse=True)
 
-    #assert(N.allclose(b,lat))
-    #assert(N.allclose(a,10))
+    #assert(np.allclose(b,lat))
+    #assert(np.allclose(a,10))
     exit()
 
     a,b = rotate_lonlat(lon,lat,angles=[(0,-10)])
-    assert(N.allclose(b,lat-10))
-    assert(N.allclose(a,0))
+    assert(np.allclose(b,lat-10))
+    assert(np.allclose(a,0))
 
-    lon = N.array([10,10,10,10])
-    lat = N.array([0,30,45,90,])
+    lon = np.array([10,10,10,10])
+    lat = np.array([0,30,45,90,])
 
 
     a,b = rotate_lonlat(lon,lat,angles=[(-10,-10),(10,0)])
-    assert(N.allclose(b,lat-10))
-    assert(N.allclose(a,lon))
+    assert(np.allclose(b,lat-10))
+    assert(np.allclose(a,lon))
 
 
 if __name__=="__main__":
