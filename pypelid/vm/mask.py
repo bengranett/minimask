a9e794c1ca46589b8fdf27c2028b0aae3f184dfb
Ben Granett <ben.granett@brera.inaf.it>
2016-05-22 10:59:22 +0200

    Update mask. Speed up loading by postponing pixelization and kdtree.  Remove testing functions.

--- pypelid/vm/mask.py
+++ pypelid/vm/mask.py
@@ -4,8 +4,6 @@ import pypelid.utils.sphere as sphere
 import pypelid.utils.misc as misc
 import pypelid.vm.healpix_projection as hp
 from sklearn.neighbors import KDTree
-import healpy
-import time
 
 SPHERE_AREA = 4*np.pi*(180/np.pi)**2
 DEG2RAD = np.pi/180
@@ -20,12 +18,11 @@ class Mask:
 		self.centers = []
 		self.costheta = []
 		self.vertices = []
+		self.lookup_tree = None
 
 		# initialize pixel mask
-		self.pixel_mask_nside = pixel_mask_nside
-		self.pixel_mask_order = pixel_mask_order
-		self.HP = hp.HealpixProjector(self.pixel_mask_nside,self.pixel_mask_order)
-		self.pixel_mask = np.zeros(self.HP.npix, dtype=bool)
+		self.grid = hp.HealpixProjector(pixel_mask_nside, pixel_mask_order)
+		self.pixel_mask = None
 
 	def import_vertices(self, polygons):
 		""" Import a mask as a list of longitude, latitude vertices representing
@@ -38,22 +35,6 @@ class Mask:
 		Outputs
 		-------
 		"""
-		self._import_vertices(polygons)
-		self.build_pixel_mask()
-
-
-	def _import_vertices(self, polygons):
-		""" Import a mask as a list of longitude, latitude vertices representing
-		convex polygons.
-
-		Inputs
-		------
-		polygons -
-
-		Outputs
-		-------
-		"""
-		t0 = time.time()
 		for vertices in polygons:
 			lon, lat = np.transpose(vertices)
 
@@ -85,30 +66,34 @@ class Mask:
 			self.centers.append(center)
 			self.costheta.append(costheta)
 			self.vertices.append(vertices)
+		logging.info("Loaded %i polygons", len(self.polygons))
 
-		print "import vert",time.time()-t0
-
-		self.centers = np.array(self.centers)
+	def _build_lookup_tree(self):
+		""" """
 		# initialize the tree data structure for quick spatial lookups.
+		logging.debug("Building mask lookup tree")
 		self.lookup_tree = KDTree(self.centers)
 		self.search_radius = np.arccos(np.min(self.costheta))
+		logging.debug("Mask search radius: %f", self.search_radius)
 
-
-	def build_pixel_mask(self, expand_fact=1):
+	def _build_pixel_mask(self, expand_fact=1):
 		""" """
-		pix = np.arange(self.HP.npix)
-		xyz = np.transpose(self.HP.pix2vec(pix))
-		radius = expand_fact * self.HP.pixel_size
+		if self.lookup_tree is None:
+			self._build_lookup_tree()
+
+		self.pixel_mask = np.zeros(self.grid.npix, dtype=bool)
+		pix = np.arange(self.grid.npix)
+		xyz = np.transpose(self.grid.pix2vec(pix))
+		radius = expand_fact * self.grid.pixel_size
 		matches = self.lookup_tree.query_radius(xyz, radius)
 		for i,m in enumerate(matches):
 			if len(m)>0:
 				self.pixel_mask[i] = True
 		self._cells = np.where(self.pixel_mask > 0)[0]
-		xyz = np.transpose(self.HP.pix2vec(self._cells))
+		xyz = np.transpose(self.grid.pix2vec(self._cells))
 		self.pixel_lookup = KDTree(xyz)
 
-
-	def write_mangle_file(self, filename):
+	def write_mangle_fits(self, filename):
 		""" Write out a file compatable with mangle.  Let's use FITS format?
 
 		Inputs
@@ -133,7 +118,7 @@ class Mask:
 				poly = self.polygons[num]
 				cm = self.cap_cm[num]
 				ncaps = len(poly)
-				out.write("polygon %i %i\n" % (num, ncaps)
+				out.write("polygon %i %i\n" % (num, ncaps))
 				for i in range(ncaps):
 					x,y,z = np.transpose(poly[i])
 					out.write("%10f %10f %10f %10f\n"%(x,y,z,cm[i]))
@@ -178,17 +163,6 @@ class Mask:
 		cap_cm.append(cm)
 		logging.info("Loaded %s and got %i polygons", filename, len(poly))
 
-	def cap_contains(self, cap, cm, points):
-		""" Check if a point is contained in a single spherical cap.
-		Inputs
-		------
-		cap - x,y,z tuple of cap center
-		cm  - Cap polar angle in Mangle notation cm = 1-cos(theta)
-		point - x,y,z tuple of point to test
-		"""
-		cd = 1 - np.dot(points, cap)
-		return cd < cm
-
 	def contains(self, lon, lat):
 		""" Check if points are inside mask.
 		Inputs
@@ -200,6 +174,9 @@ class Mask:
 		-------
 		bool array
 		"""
+		if self.lookup_tree is None:
+			self._build_lookup_tree()
+
 		lon = np.array(lon)
 		lat = np.array(lat)
 
@@ -224,40 +201,6 @@ class Mask:
 					break
 		return inside
 
-	def _select_cells_check(self, coarse_cell, coarse_nside, coarse_order):
-		""" Returns list of cells in the internal healpix map that fall in a
-		given patch of sky.  The resoultion of the internal map must be higher
-		than the coarse resolution input here.
-
-		Inputs
-		------
-		coarse_cell - cell number or list defining patch of sky
-		coarse_nside - nside of pixelization
-		coarse_order - pixelization order
-
-		Outputs
-		-------
-		list of cells in pixel map
-		"""
-		assert(self.pixel_mask_nside > coarse_nside)
-		t0 = time.time()
-		if misc.is_number(coarse_cell):
-			coarse_cell = [int(coarse_cell)]
-		print coarse_cell
-
-		clon,clat = self.HP.pix2ang(self._cells)
-
-		# compute pixel numbers on coarser grid
-		coarse_grid = hp.HealpixProjector(coarse_nside, coarse_order)
-		id_coarse = coarse_grid.ang2pix(clon,clat)
-
-		matches = []
-		for p in coarse_cell:
-			sel = np.where(id_coarse == p)
-			matches.append(self._cells[sel])
-		matches = np.concatenate(matches)
-		print "select cells",time.time()-t0
-		return matches
 
 	def _select_cells(self, coarse_cell, coarse_nside, coarse_order):
 		""" Returns list of cells in the internal healpix map that fall in a
@@ -274,8 +217,13 @@ class Mask:
 		-------
 		list of cells in pixel map
 		"""
-		assert(self.pixel_mask_nside > coarse_nside)
-		t0 = time.time()
+		# If the resolution given is higher than the internal pixel map,
+		# rebuild the map at an even higher resolution.
+		if coarse_nside > self.grid.nside:
+			self.grid = hp.HealpixProjector(2 * coarse_nside, coarse_order)
+			self._build_pixel_mask()
+
+		# make sure input is iterable
 		if misc.is_number(coarse_cell):
 			coarse_cell = [int(coarse_cell)]
 
@@ -288,9 +236,8 @@ class Mask:
 
 		xyz = np.take(self.pixel_lookup.data,matches,axis=0)
 		pix = coarse_grid.vec2pix(*xyz.transpose())
-		ii = pix==coarse_cell
+		ii = pix == coarse_cell
 		matches = self._cells[matches[ii]]
-		print "select cells fast",time.time()-t0
 		return matches
 
 	def draw_random_position(self, dens, cell=None, nside=1, order='ring'):
@@ -315,14 +262,18 @@ class Mask:
 		-------
 		ra, dec
 		"""
+		if self.pixel_mask is None:
+			self._build_pixel_mask()
+
 		if cell is None:
 			cell = self._cells   # full sky
 		else:
 			# sample only selected patches defined by a healpix cell
-			cell = self._select_cells(cell,nside,order)
+			cell = self._select_cells(cell, nside, order)
 
 		if len(cell)==0:
 			# if there are no cells return empty arrays
+			logging.warning("Effective area of the survey is 0.")
 			return np.array([]),np.array([])
 
 		if misc.is_number(cell):
@@ -331,41 +282,11 @@ class Mask:
 		else:
 			n_cells = len(cell)
 
-		n = int(SPHERE_AREA * 1. / self.HP.npix * n_cells * dens)
+		n = int(SPHERE_AREA * 1. / self.grid.npix * n_cells * dens)
 
-		lon,lat = self.HP.random_sample(cell, n)
-
-		sel = self.contains(lon,lat)
-		print "eff",np.sum(sel)*1./len(sel)
-
-		return lon[sel],lat[sel]
-
-	def draw_random_position_slow(self, dens, cell=None, nside=1, order='ring'):
-		""" This routine is here for testing purposes.  It samples from the full
-		sphere or from a given healpix cell without further optimization."""
-		if cell is None:
-			n = int(SPHERE_AREA * dens)
-			lon,lat = sphere.sample_sphere(n)
-			sel = self.contains(lon,lat)
-			print "eff",np.sum(sel)*1./len(sel)
-			return lon[sel],lat[sel]
-
-		if misc.is_number(cell):
-			n_cells = 1
-		else:
-			n_cells = len(cell)
-
-		HP = hp.HealpixProjector(nside, order)
-		n = int(SPHERE_AREA * 1. / HP.npix * n_cells * dens)
-
-		lon,lat = HP.random_sample(cell, n)
+		lon,lat = self.grid.random_sample(cell, n)
 
 		sel = self.contains(lon,lat)
+		logging.debug("Random sampling success rate: %f",np.sum(sel)*1./len(sel))
 
-		print "eff",np.sum(sel)*1./len(sel)
 		return lon[sel],lat[sel]
-
-
-
-if __name__=="__main__":
-	test()
